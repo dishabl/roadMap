491c5ee85113466b0cfa8f3d552475f5f65ac46a
dishabl <85846973+dishabl@users.noreply.github.com>
2023-09-24 01:24:29 +0300

    Update AppHeader.js

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function AppHeader() {
   return (
     <div className="App-header">
       <div className="Header">
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
         {/* <h5>Основные темы для изучения библиотеки React</h5> */}
         <div className="info">Теория по библиотеке React</div>
         <img src={logo} className="App-logo" alt="logo" />
